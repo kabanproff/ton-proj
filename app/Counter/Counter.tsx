0e3fc4caf35f34ad653fa8a9c1e2501623b4f482
Artyom Pratsko <kabanproff@gmail.com>
2024-05-08 17:08:32 +0300

    edit in ganbei

--- app/Counter/Counter.tsx
+++ app/Counter/Counter.tsx
@@ -5,14 +5,44 @@ import { useState, useEffect } from 'react'
 import styles from "./counter.module.css";
 import { useTonConnect } from '../hooks/useTonConnect';
 
-const Counter = () => {
+const Counter = ({handlerActive, active, counter,isZerable, setCounter}) => {
   const { network } = useTonConnect();
-  console.log(network)
+  // console.log(network)
   const [count, setCount] = useState(0)
-  const [data, setData] = useState(0)
+  const [data, setData] = useState(0);
+  const [timer, setTimer] = useState(0)
 
   async function getData(){
     // return await main();
+  }
+
+  const handlerClick = () => {
+    // let timer;
+    if((counter < 100) && isZerable) {
+      clearInterval(timer)
+      console.log('ddddd',timer)
+      setTimer(setInterval(
+        () =>
+          setCounter((prevCount) => {
+            if((prevCount <= 50) && prevCount < 50) {
+              // console.log(timer)
+              return prevCount + 1; 
+            } else {
+              // console.log(timer)
+              clearInterval(timer);
+              // setActive(false)
+              // setIsZerable(true)
+              return 50;
+            }
+          }
+          ),1000))
+      // setTimer(timer);
+      return;
+    }
+    handlerActive(true);
+    if(active) {
+      setCount(count + 1);
+    }
   }
   
   // useEffect(()=>{
@@ -38,7 +68,7 @@ const Counter = () => {
       <p>You clicked {count} stars</p>
       <p>tons - {data} </p>
 
-      <button className={styles.btn} onClick={() => setCount(count + 1)}>Click me</button>
+      <button className={styles.btn} onClick={handlerClick}>Click me</button>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,67 @@
+'use client'
 import styles from "./page.module.css";
 import Counter from "./Counter/Counter";
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
+  
+  let incrementBy = 1;
+  let timeInterval = 100;
+  
+  const [count, setCount] = useState(50);
+  const [timer, setTimer] = useState(0);
+  const [active, setActive] = useState(false);
+  const [isZerable, setIsZerable] = useState(false)
+  
+  useEffect(() => {
+    let timer = 0;
+    if(active && count > 0) {
+      clearInterval(timer)
+      // console.log(active, count, 'd',timer)
+      timer = setInterval(
+        () =>
+          setCount((prevCount) =>{
+            if((prevCount <= 50) && prevCount > 0) {
+              console.log(timer)
+              return prevCount - incrementBy 
+            } else {
+              console.log(timer)
+              clearInterval(timer);
+              setActive(false)
+              setIsZerable(true)
+              return 0;
+            }
+          }
+          ),
+        timeInterval
+      );
+      setTimer(timer);
+      
+    } else {
+      console.log('active', active)
+    
+      // 
+      // setCount(50) 
+      // setActive(false)
+    }
+  }, [active]);
+
 
 
   return (
     <main className={styles.main}>
       <h1>Counter</h1>
-      <Counter />
+      <Counter 
+        handlerActive={setActive} 
+        active={active} 
+        counter={count} 
+        isZerable={isZerable}
+        setCounter={setCount}
+      />
+      <label htmlFor="file">File progress:</label>
+      <progress id="file" max="50" value={count}></progress>
+      <span>{count}</span>
     </main>
   );
 }
